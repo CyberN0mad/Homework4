be7a3c30b24ab1856595498ea1603580cd14a35f
cyberN0mad <s.baitur@mail.ru>
2020-09-17 01:48:48 +0600

    Dopolnitelnoe zadanie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,18 @@ package com.company;
 
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
-    public static int bossHealth = 700;
+    public static int bossHealth = 1500;
     public static int bossDamage = 50;
     public static String bossDefence = "";
-    public static int[] heroesHealth = {270, 260, 250, 240, 250};
-    public static int[] heroesDamage = {25, 15, 20, 30, 20};
+    public static int[] heroesHealth = {270, 260, 250, 240, 250, 100, 500};
+    public static int[] heroesDamage = {25, 15, 20, 30, 20, 50, 5};
     public static String[] heroesAttackType = {
-            "Physical", "Magical", "Kinetic", "Archer", "Medic"};
+            "Physical", "Magical", "Kinetic", "Archer", "Medic", "Lucky", "Golem"};
     public static int roundCounter = 0;
 
     public static void main(String[] args) {
@@ -28,6 +29,7 @@ public class Main {
         int randomIndex = r.nextInt(heroesAttackType.length); // 0,1,2,3
         bossDefence = heroesAttackType[randomIndex];
         System.out.println("Boss choose: " + bossDefence);
+
     }
 
     public static boolean isGameOver() {
@@ -58,15 +60,46 @@ public class Main {
     }
 
     public static void bossHits() {
-        for (int i = 0; i < heroesDamage.length; i++) {
-            if (heroesHealth[i] > 0) {
-                if (heroesHealth[i] - bossDamage < 0) {
+        if (heroesHealth[6] > 0) {                                                                                      // Poluchenie urona Golemom
+            heroesHealth[6] -= bossDamage + bossDamage / 5 * heroesHealth.length - 1;
+            if (heroesHealth[6] < 0) {
+                heroesHealth[6] = 0;
+            }
+            System.out.println("Golem absorbed: " + (bossDamage + bossDamage / 5 * heroesHealth.length - 1));
+            System.out.println("Golem health: " + heroesHealth[6]);
+        }
+
+        Random random = new Random();
+        int coefForLucky = random.nextInt(5);                                                                     //это относится к Lucky
+
+
+        for (int i = 0; i < heroesDamage.length - 1; i++) {
+            if (heroesHealth[6] > 0) {
+                if (heroesHealth[i] - (bossDamage - bossDamage / 5) < 0) {
                     heroesHealth[i] = 0;
                 } else {
-                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                    heroesHealth[i] = heroesHealth[i] - (bossDamage - bossDamage / 5);
+                    if (coefForLucky == 4) {
+                        heroesHealth[5] += (bossDamage - bossDamage / 5);
+                    }
+                }
+            }
+            else {
+                if (heroesHealth[i] - bossDamage < 0) {                                                                 // Posle smerti Golema
+                    heroesHealth[i] = 0;
+                } else {
+                    heroesHealth[i] -= bossDamage;
+                    if (coefForLucky == 4) {
+                        heroesHealth[5] += bossDamage;
+                    }
                 }
             }
         }
+        if (coefForLucky == 4) {                                                                                        // Вывод Lucky
+            System.out.println("Lucky dodged!!!");
+        }
+
+
     }
 
     public static void MedicHeals() {
@@ -90,6 +123,7 @@ public class Main {
 
 
     public static void heroesHit() {
+
         for (int i = 0; i < heroesHealth.length - 1; i++) {
             if (heroesHealth[i] > 0 && bossHealth > 0) {
                 if (heroesAttackType[i] == bossDefence) {
